189566794f4170adcaa4cf118a1e2ff61c730cee
fish-dt <fissehadesta70@gmail.com>
2025-05-02 13:26:46 +0300

    Update AndroidManifest and Home Activity layout: Changed main activity to HomeActivity, added data extraction rules, and modified layout properties including background color and button attributes.

---
+++ app/src/main/java/com/example/quizapp/HomeActivity.java
@@ -0,0 +1,25 @@
+package com.example.quizapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, CategoryActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+} 
